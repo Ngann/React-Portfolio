9adc7165e27e1e7ef3cfce0d918da8e04cc4e16e
Ngan Nguyen <ngancamnguyen@gmail.com>
2019-02-15 20:29:46 -0800

    add projects to list

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -1,36 +1,76 @@
 import React from 'react';
 import Project from './Project';
+import Grid from '@material-ui/core/Grid';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  image: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    border:'1px solid black'
+  },
+  title: {
+    justify: 'center',
+    textAlign: 'center',
+    border:'1px solid black'
+  },
+});
 
 var masterProjectList = [
   {
-    names: 'Thato and Haley',
-    location: '3A',
+    names: 'JavaScipt/React',
+    location: 'Portfolio',
     issue: 'Firebase won\'t save record. Halp.'
   },
   {
-    names: 'Sleater and Kinney',
-    location: '4B',
+    names: 'TypeScript/Angular',
+    location: 'Social Media',
     issue: 'Fox image not displaying on page, can only see duck?'
   },
   {
-    names: 'Imani & Jacob',
-    location: '9F',
+    names: 'Ruby/Rails/Postgres',
+    location: 'BattleShip',
+    issue: 'Donkey picture not displaying on hover in Zoology app. :('
+  },
+  {
+    names: 'JavaScipt/Firebase',
+    location: 'Other',
     issue: 'Donkey picture not displaying on hover in Zoology app. :('
   }
 ];
 
-function ProjectList(){
+function ProjectList(props){
+  const { classes } = props;
   return (
-    <div>
+    <div className={classes.root}>
+      <Grid item xs={12}>
+        <Typography variant="h2" gutterBottom className={classes.title}>
+          Projects
+        </Typography>
+      </Grid >
       <hr/>
+      <Grid container spacing={12}>
       {masterProjectList.map((ticket, index) =>
-        <Project names={ticket.names}
-          location={ticket.location}
-          issue={ticket.issue}
-          key={index}/>
+        <Grid xs={6} s={4}>
+          <Project names={ticket.names}
+            location={ticket.location}
+            issue={ticket.issue}
+            key={index}/>
+        </Grid>
       )}
+      </Grid>
     </div>
   );
 }
 
-export default ProjectList;
+ProjectList.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ProjectList);
