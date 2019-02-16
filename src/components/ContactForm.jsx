5be946d954fd2b47453424dfa1c1e5f4324e819d
Ngan Nguyen <ngancamnguyen@gmail.com>
2019-02-15 21:06:23 -0800

    update form fields

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,25 +1,116 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import AutoScale from 'react-auto-scale';
+import classNames from 'classnames';
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
 
-function ContactForm(){
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  image: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+  pic: {
+    width: '100%'
+  },
+  title: {
+    justify: 'center',
+    textAlign: 'center',
+    border:'1px solid black'
+  },
+  box: {
+    width: '80%',
+    margin: 'auto',
+    color: 'white',
+    border:'1px solid black'
+  },
+  social: {
+    width: '50%',
+    margin: 'auto',
+    color: 'red',
+    border:'1px solid black',
+    textAlign: 'center',
+  }
+});
+
+function Resume(props) {
+  const { classes } = props;
+
   return (
-    <div>
-      <form>
-        <input
-          type='text'
-          id='names'
-          placeholder='Pair Names'/>
-        <input
-          type='text'
-          id='location'
-          placeholder='Location'/>
-        <textarea
-          id='issue'
-          placeholder='Describe your issue.'/>
-        <button type='submit'>Help!</button>
-      </form>
+    <div className={classes.root}>
+      <Grid container spacing={24}>
+        <Grid item xs={12}>
+          <Typography variant="h2" gutterBottom className={classes.title}>
+            Contact
+          </Typography>
+        </Grid >
+      </Grid >
+      <Grid container className={classes.box}>
+        <Grid item xs={24}>
+          <form className={classes.container} noValidate autoComplete="off">
+            <TextField
+              id="filled-name"
+              label="Name"
+              className={classes.textField}
+              value=""
+              margin="normal"
+              variant="filled"
+            />
+
+            <TextField
+              required
+              id="filled-email-input"
+              label="Email"
+              className={classes.textField}
+              type="email"
+              name="email"
+              autoComplete="email"
+              margin="normal"
+              variant="filled"
+            />
+
+            <TextField
+              id="filled-number"
+              label="Number"
+              value=" "
+              type="number"
+              className={classes.textField}
+              InputLabelProps=" "
+              margin="normal"
+              variant="filled"
+            />
+
+            <TextField
+              id="filled-multiline-static"
+              label="Message"
+              multiline
+              rows="4"
+              defaultValue=" "
+              className={classes.textField}
+              margin="normal"
+              variant="filled"
+            />
+            <button type='submit'>Send</button>
+          </form>
+        </Grid>
+      </Grid>
     </div>
   );
 }
+Resume.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
 
-export default ContactForm;
+export default withStyles(styles)(Resume);
